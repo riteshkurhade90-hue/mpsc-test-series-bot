f9780c98935f3aa0ad6adbfd63eaaa93f59f61ac
riteshkurhade90-hue <riteshkurhade90@gmail.com>
2026-07-02 19:40:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+SHEET_ID = os.getenv("SHEET_ID")
+WORKSHEET_NAME = "Questions"
+
+QUIZ_DURATION_HOURS = 2
+TOTAL_QUESTIONS = 15
