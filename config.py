e1809c3831bbee9a8ded4d76c422ee315b673ffa
riteshkurhade90-hue <riteshkurhade90@gmail.com>
2026-07-03 00:07:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 import os
+import json
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
 SHEET_ID = os.getenv("SHEET_ID")
-WORKSHEET_NAME = "Questions"
 
-QUIZ_DURATION_HOURS = 2
+GOOGLE_CREDENTIALS = json.loads(
+    os.getenv("GOOGLE_CREDENTIALS")
+)
+
 TOTAL_QUESTIONS = 15
+QUIZ_DURATION = 120
