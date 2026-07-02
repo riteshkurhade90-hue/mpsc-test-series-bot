3f37f1b8e15af44b50bf3a6b42b73ff96fd77889
riteshkurhade90-hue <riteshkurhade90@gmail.com>
2026-07-03 00:08:07 +0530

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,1 +1,22 @@
+import json
+import gspread
+from google.oauth2.service_account import Credentials
 
+from config import SHEET_ID, GOOGLE_CREDENTIALS
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+credentials = Credentials.from_service_account_info(
+    GOOGLE_CREDENTIALS,
+    scopes=SCOPES
+)
+
+client = gspread.authorize(credentials)
+
+spreadsheet = client.open_by_key(SHEET_ID)
+
+questions_sheet = spreadsheet.worksheet("Questions")
+results_sheet = spreadsheet.worksheet("Results")
