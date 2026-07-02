92bf73763f958373d0c53cae40ae869f333d0374
riteshkurhade90-hue <riteshkurhade90@gmail.com>
2026-07-03 00:08:52 +0530

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -20,3 +20,47 @@ spreadsheet = client.open_by_key(SHEET_ID)
 
 questions_sheet = spreadsheet.worksheet("Questions")
 results_sheet = spreadsheet.worksheet("Results")
+def get_questions(day):
+    rows = questions_sheet.get_all_records()
+
+    questions = []
+
+    for row in rows:
+        if int(row["Day"]) == int(day):
+            questions.append(row)
+
+    return questions
+
+
+def save_result(user_id, name, username, day, correct, wrong, percentage, time_taken, submitted_at):
+    results_sheet.append_row([
+        user_id,
+        name,
+        username,
+        day,
+        correct,
+        wrong,
+        percentage,
+        time_taken,
+        submitted_at
+    ])
+
+
+def get_top3(day):
+    rows = results_sheet.get_all_records()
+
+    filtered = []
+
+    for row in rows:
+        if int(row["Day"]) == int(day):
+            filtered.append(row)
+
+    filtered.sort(
+        key=lambda x: (
+            -int(x["Correct"]),
+            -float(x["Percentage"]),
+            float(x["Time Taken (sec)"])
+        )
+    )
+
+    return filtered[:3]
