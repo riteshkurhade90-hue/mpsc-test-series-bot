b07d26f1af17fab655a5636627cf48776d6826d1
riteshkurhade90-hue <riteshkurhade90@gmail.com>
2026-07-02 19:41:11 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,32 @@
+import sqlite3
 
+DB_NAME = "quiz.db"
+
+
+def connect():
+    return sqlite3.connect(DB_NAME)
+
+
+def create_tables():
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        name TEXT
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS results (
+        user_id INTEGER PRIMARY KEY,
+        correct INTEGER,
+        wrong INTEGER,
+        percentage REAL,
+        submitted_at TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
