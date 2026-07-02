3b9176708d8a79466ed060924597356ebdc642b1
riteshkurhade90-hue <riteshkurhade90@gmail.com>
2026-07-03 00:10:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,41 @@
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
 
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    ContextTypes
+)
+
+from config import BOT_TOKEN
+from sheets import get_questions
+
+# User Session
+user_sessions = {}
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "📝 Start Today's Test",
+                callback_data="start_test"
+            )
+        ]
+    ]
+
+    await update.message.reply_text(
+        "🎯 Welcome to MPSC Test Series\n\n"
+        "Click the button below to start today's test.",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
